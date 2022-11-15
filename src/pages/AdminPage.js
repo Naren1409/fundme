72e5ff39024797ee4c620016a69ae73ab2a331b8
neelmani098 <neelmani98@outlook.com>
2022-11-15 13:41:02 +0530

    fixed admin route

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -302,6 +302,7 @@ const AdminPage = () => {
                     {/*) : null}*/}
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                       <p className="text-gray-900 whitespace-no-wrap uppercase">
+                        <span>&#36; </span>
                         {campaign.campaignTotalAmount}
                       </p>
                     </td>
