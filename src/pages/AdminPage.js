16cbd216565177b9b0d608260443ee23a9777bd9
neelmani098 <neelmani98@outlook.com>
2022-11-15 14:35:59 +0530

    fixed admin page footer white space

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -100,31 +100,11 @@ const AdminPage = () => {
           status: "rejected",
           rejectReason: rejectReason,
         });
-        // .then((response) => {
-        //   if (response.status == 200) {
-        //     swal("Yayy!, Campaign has been approved and now being active", {
-        //       icon: "success",
-        //     });
-        //   } else {
-        //     swal("Some error occurred, Please try again!", {
-        //       icon: "error",
-        //     });
-        //   }
-        // });
       })
       .then((results) => {
         return results.data;
       })
       .then((json) => {
-        // const movie = json.results[0];
-        //
-        // if (!movie) {
-        //   return swal("No movie was found!");
-        // }
-        //
-        // const name = movie.trackName;
-        // const imageURL = movie.artworkUrl100;
-
         const newCampaigns = campaigns.map((obj) => {
           if (obj.campaignId === json.campaignId) {
             return { ...obj, status: json.status };
@@ -162,7 +142,6 @@ const AdminPage = () => {
           onChange={campaignStateChangeHandler}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-          {/*<option selected="">Choose a State</option>*/}
           <option value="all">All</option>
           <option value="inreview">In Review</option>
           <option value="goalReached">Goal Reached</option>
@@ -212,14 +191,6 @@ const AdminPage = () => {
                   >
                     Status
                   </th>
-                  {/*{campaign.status === "rejected" ? (*/}
-                  {/*  <th*/}
-                  {/*    scope="col"*/}
-                  {/*    className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"*/}
-                  {/*  >*/}
-                  {/*    Reason*/}
-                  {/*  </th>*/}
-                  {/*) : null}*/}
                   <th
                     scope="col"
                     className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
@@ -235,127 +206,130 @@ const AdminPage = () => {
                 </tr>
               </thead>
               <tbody>
-                {campaignsToDisplay.map((campaign) => (
-                  <tr key={campaign.campaignId}>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div className="flex items-center">
-                        <div className="ml-3">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            {campaign.createdBy}
-                          </p>
+                {campaignsToDisplay.length ? (
+                  campaignsToDisplay.map((campaign) => (
+                    <tr key={campaign.campaignId}>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div className="flex items-center">
+                          <div className="ml-3">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {campaign.createdBy}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {campaign.campaignName}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {moment(campaign.createdAt).format("LL")}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {moment(campaign.campaignLastDate).format("LL")}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap uppercase">
-                        {campaign.status === "goalReached" ? (
-                          <span
-                            className={`${statusContainerClass} text-blue-500 border border-blue-500 bg-blue-100 uppercase`}
-                          >
-                            {`GOAL REACHED`}
-                          </span>
-                        ) : null}
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {campaign.campaignName}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {moment(campaign.createdAt).format("LL")}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {moment(campaign.campaignLastDate).format("LL")}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap uppercase">
+                          {campaign.status === "goalReached" ? (
+                            <span
+                              className={`${statusContainerClass} text-blue-500 border border-blue-500 bg-blue-100 uppercase`}
+                            >
+                              {`GOAL REACHED`}
+                            </span>
+                          ) : null}
+                          {campaign.status === "inreview" ? (
+                            <span
+                              className={`${statusContainerClass} text-yellow-500 border border-yellow-500 bg-yellow-100 uppercase`}
+                            >
+                              {`IN REVIEW`}
+                            </span>
+                          ) : null}
+                          {campaign.status === "rejected" ? (
+                            <span
+                              className={`${statusContainerClass} text-red-500 border border-red-500 bg-red-100 uppercase`}
+                            >
+                              {`REJECTED`}
+                            </span>
+                          ) : null}
+                          {campaign.status === "active" ? (
+                            <span
+                              className={`${statusContainerClass} text-green-500 border border-green-500 bg-green-100 uppercase`}
+                            >
+                              {`ACTIVE`}
+                            </span>
+                          ) : null}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap uppercase">
+                          <span>&#36; </span>
+                          {campaign.campaignTotalAmount}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                         {campaign.status === "inreview" ? (
-                          <span
-                            className={`${statusContainerClass} text-yellow-500 border border-yellow-500 bg-yellow-100 uppercase`}
-                          >
-                            {`IN REVIEW`}
-                          </span>
-                        ) : null}
-                        {campaign.status === "rejected" ? (
-                          <span
-                            className={`${statusContainerClass} text-red-500 border border-red-500 bg-red-100 uppercase`}
-                          >
-                            {`REJECTED`}
-                          </span>
-                        ) : null}
-                        {campaign.status === "active" ? (
-                          <span
-                            className={`${statusContainerClass} text-green-500 border border-green-500 bg-green-100 uppercase`}
-                          >
-                            {`ACTIVE`}
-                          </span>
-                        ) : null}
-                      </p>
-                    </td>
-                    {/*{campaign.status === "rejected" ? (*/}
-                    {/*  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">*/}
-                    {/*    <p className="text-gray-900 whitespace-no-wrap uppercase">*/}
-                    {/*      {campaign.reject_reason}*/}
-                    {/*    </p>*/}
-                    {/*  </td>*/}
-                    {/*) : null}*/}
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap uppercase">
-                        <span>&#36; </span>
-                        {campaign.campaignTotalAmount}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      {campaign.status === "inreview" ? (
-                        <div>
-                          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                            <span
-                              aria-hidden="true"
-                              className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                            ></span>
-                            <span
-                              className="relative"
-                              onClick={() => {
-                                approveHandler(campaign.campaignId);
-                              }}
-                            >
-                              Approve
+                          <div>
+                            <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                              <span
+                                aria-hidden="true"
+                                className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                              ></span>
+                              <span
+                                className="relative"
+                                onClick={() => {
+                                  approveHandler(campaign.campaignId);
+                                }}
+                              >
+                                Approve
+                              </span>
                             </span>
-                          </span>
-                          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                            <span
-                              aria-hidden="true"
-                              className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                            ></span>
-                            <span
-                              className="relative"
-                              onClick={() => {
-                                rejectHandler(campaign.campaignId);
-                              }}
-                            >
-                              Reject
+                            <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                              <span
+                                aria-hidden="true"
+                                className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                              ></span>
+                              <span
+                                className="relative"
+                                onClick={() => {
+                                  rejectHandler(campaign.campaignId);
+                                }}
+                              >
+                                Reject
+                              </span>
                             </span>
+                          </div>
+                        ) : null}
+                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                          <span
+                            aria-hidden="true"
+                            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                          ></span>
+                          <span
+                            className="relative"
+                            onClick={() => {
+                              viewMoreHandler(campaign);
+                            }}
+                          >
+                            View
                           </span>
-                        </div>
-                      ) : null}
-                      <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                        <span
-                          aria-hidden="true"
-                          className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                        ></span>
-                        <span
-                          className="relative"
-                          onClick={() => {
-                            viewMoreHandler(campaign);
-                          }}
-                        >
-                          View
                         </span>
-                      </span>
-                    </td>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <div className="m-auto">
+                      <p className="items-center whitespace-no-wrap text-blue-900 uppercase">
+                        No campaigns here to display
+                      </p>
+                    </div>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
           </div>
@@ -380,8 +354,10 @@ const AdminPage = () => {
         </Modal>
       ) : null}
 
-      {tableView}
-      <Footer />
+      <div className="flex min-h-screen flex-col justify-between">
+        {tableView}
+        <Footer />
+      </div>
     </AnimationRevealPage>
   );
 };
